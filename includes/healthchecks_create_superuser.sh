fa9099c4f27f9580140f7f4a5f440f9ef4ef3011
Alexander Trost <galexrt@googlemail.com>
2016-12-16 11:56:11 +0100

    Fixed typo

--- includes/healthchecks_create_superuser.sh
+++ includes/healthchecks_create_superuser.sh
@@ -2,4 +2,4 @@
 
 cd /healthchecks || exit 1
 
-su healthcheck -c 'source /healthchecks/hc-venv/bin/activate;/healthchecks/manage.py createsuperuser'
+su healthchecks -c 'source /healthchecks/hc-venv/bin/activate;/healthchecks/manage.py createsuperuser'
